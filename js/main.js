769201dacece86bb7f649a2c5f3b6d4cc0b6d170
Farhan_Ali <fa7265522@gmail.com>
2026-01-14 13:30:39 +0530

    Added New Section on Home Page

--- js/main.js
+++ js/main.js
@@ -136,6 +136,20 @@ if (testimonialNextBtn && testimonialPrevBtn && testimonialSlides.length) {
   setInterval(nextTestimonial, 5000);
 }
 
+document.querySelectorAll(".faq-question").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const item = btn.parentElement;
+
+    document.querySelectorAll(".faq-item").forEach((faq) => {
+      if (faq !== item) {
+        faq.classList.remove("active");
+      }
+    });
+
+    item.classList.toggle("active");
+  });
+});
+
 /* ================= INIT ================= */
 document.addEventListener("DOMContentLoaded", () => {
   includeHTML(() => {
